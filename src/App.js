55db0ea344d2326d237b6c4815a44cbc79ff1182
Rūta Šipkauskaitė <s033859@ad.viko.lt>
2020-12-07 23:07:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      search: "",
+      filterPrice: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
